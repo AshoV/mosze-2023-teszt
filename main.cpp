531713bcfa031200501fa37d989c61b35a6f2d93
kocsiskrisztian <kkriszt99@gmail.com>
2023-09-25 18:30:01 +0200

    mistakes_commented

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; 
-    std::cout << '1-100 ertekek duplazasa' 
-    for (int i = 0;) 
+    int *b = new int[NELEMENTS]; //N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa' //"1-100 ertekek duplazasa"
+    for (int i = 0;) //rossz ciklusfeltetel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) 
+    for (int i = 0; i; i++) //rossz ciklusfeltetel
     {
-        std::cout << "Ertek:" 
+        std::cout << "Ertek:" // ; endl
     }    
     std::cout << "Atlag szamitasa: " << std::endl; 
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)   
+    for (int i = 0; i < N_ELEMENTS, i++)   // , kell ; helyett
     {
-        atlag += b[i] 
+        atlag += b[i] //pontosvesszo hianyzik
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
